d7b1c7b8aa43759e4f67752270bdb494caffd54c
Elghosthub <ana.elghostahmed456@gmail.com>
2024-12-08 01:19:59 +0200

    Add project files.

---
+++ tasks.lab1/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+#region Posotive and Negative number
+//Console.Write("Enter a Number: ");
+//float num = float.Parse(Console.ReadLine());
+//if(num == 0 )
+//    Console.WriteLine("This is a Zero");
+//else if (num < 0)
+//    Console.WriteLine("Negative");
+//else 
+//    Console.WriteLine("Positive");
+
+#endregion
+
+#region Even or Odd number
+//Console.Write("Enter a Number: ");
+
+//int num = int.Parse(Console.ReadLine());
+
+//if (num % 2 == 0)
+//    Console.WriteLine("Even");
+//else Console.WriteLine("Odd");
+#endregion
+
+#region Square or Rectangle
+//Console.Write("Enter length: ");
+//int len = int.Parse(Console.ReadLine()); 
+//Console.Write("Enter Width: ");
+//int wid = int.Parse(Console.ReadLine());
+
+//if (wid == len)
+//    Console.WriteLine("This is a Square");
+//else 
+//    Console.WriteLine("This is a Rectangle");
+#endregion
