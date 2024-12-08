31deed62fe42ac4df79a43a0b3016d9dab0883e4
Elghosthub <ana.elghostahmed456@gmail.com>
2024-12-08 02:31:54 +0200

    Completed all tasked and added them to the file here again but i noticed there is two problems repeated i seperated every problem in a region

--- tasks.lab1/Program.cs
+++ tasks.lab1/Program.cs
@@ -1,4 +1,95 @@
 ﻿
+#region print, name age and hobby
+//Console.WriteLine("My name is Ahmed Abdelrahman Elgamal ");
+//Console.WriteLine("I am 25 years old ");
+//Console.WriteLine("I love Coding ");
+#endregion
+
+#region dataTypes
+//int num;
+//double num2;
+//string str;
+//bool b;
+//num = 4;
+//num2 = 123456;
+//str = "Hello, World!";
+//b= true;
+//Console.WriteLine(num);
+//Console.WriteLine(num2);
+//Console.WriteLine(str);
+//Console.WriteLine(b);
+#endregion
+
+#region convert temperature c to f
+//Console.Write("Enter a Celsuis: ");
+//int c = int.Parse(Console.ReadLine());
+//int f = c * (9 / 5) + 32;
+//Console.WriteLine(f);
+#endregion
+
+#region Operations +, -, *, /
+
+//Console.Write("Enter the First Number: ");
+//float num1 = float.Parse(Console.ReadLine());
+//Console.Write("Enter the Second Number: ");
+//float num2 = float.Parse(Console.ReadLine());
+//float sum = num1+ num2;
+//Console.WriteLine("the sumation is " + sum);
+//float sub = num1 - num2;
+//Console.WriteLine("the substaraction is " + sub);
+//float multi = num1 * num2;
+//Console.WriteLine("the Multiblacation is " + multi);
+//float div = num1 / num2;
+//Console.WriteLine("the divsion is " + div);
+
+#endregion
+
+#region Area of the Rectangle
+//Console.Write("Enter length: ");
+//int len = int.Parse(Console.ReadLine());
+//Console.Write("Enter Width: ");
+//int wid = int.Parse(Console.ReadLine());
+//int area = len * wid;
+//Console.WriteLine("The Area is "+ area);
+#endregion
+
+#region Even or Odd number
+//Console.Write("Enter a Number: ");
+
+//int num = int.Parse(Console.ReadLine());
+
+//if (num % 2 == 0)
+//    Console.WriteLine("Even");
+//else Console.WriteLine("Odd");
+#endregion
+
+#region Check Age
+//Console.Write("Enter Your Age: ");
+//int age = int.Parse(Console.ReadLine());
+//if (age < 18)
+//    Console.WriteLine("You are not eligible to vote.");
+//else
+//    Console.WriteLine("You are eligible to vote.");
+#endregion
+
+#region Grade System
+//Console.Write("Enter a Score 0-100: ");
+//int score = int.Parse(Console.ReadLine());
+//if( score < 0)
+//    Console.WriteLine("a minus plz no one that stuipd.");
+//else if (score < 60)
+//    Console.WriteLine('F');
+//else if (score < 70)
+//    Console.WriteLine('D');
+//else if (score < 80)
+//    Console.WriteLine('C');
+//else if (score < 90)
+//    Console.WriteLine('B');
+//else if (score <= 100)
+//    Console.WriteLine('A');
+//else
+//    Console.WriteLine("Enter a score between 0 - 100");
+#endregion
 
 #region Posotive and Negative number
 //Console.Write("Enter a Number: ");
@@ -12,7 +103,7 @@
 
 #endregion
 
-#region Even or Odd number
+#region Even or Odd number again huh
 //Console.Write("Enter a Number: ");
 
 //int num = int.Parse(Console.ReadLine());
@@ -33,3 +124,4 @@
 //else 
 //    Console.WriteLine("This is a Rectangle");
 #endregion
+
